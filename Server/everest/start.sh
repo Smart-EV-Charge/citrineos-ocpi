f7b6a5e8415ec13dd32a66b232a9f5204e721d33
Amirhossein Mahmoudi <am.mahmoudi@outlook.com>
2026-02-27 20:35:32 +0330

    refactor(start.sh): streamline OCPP version handling and script execution

--- Server/everest/start.sh
+++ Server/everest/start.sh
@@ -3,5 +3,17 @@
 # SPDX-License-Identifier: Apache-2.0
 
 http-server /tmp/everest_ocpp_logs -p 8888 &
-chmod +x /ext/source/build/run-scripts/run-sil-ocpp201-pnc.sh
-/ext/source/build/run-scripts/run-sil-ocpp201-pnc.sh
+
+if [ "$OCPP_VERSION" = "one" ]; then
+	chmod +x /ext/source/build/run-scripts/run-sil-ocpp.sh
+	sed -i "0,/127.0.0.1:8180\/steve\/websocket\/CentralSystemService\// s|127.0.0.1:8180/steve/websocket/CentralSystemService/|${EVEREST_TARGET_URL}|" /ext/source/build/dist/share/everest/modules/OCPP/config-docker.json
+	/ext/source/build/run-scripts/run-sil-ocpp.sh
+else
+	if [ "$EVEREST_ENABLE_PNC" = "true" ]; then
+		chmod +x /ext/source/build/run-scripts/run-sil-ocpp201-pnc.sh
+		/ext/source/build/run-scripts/run-sil-ocpp201-pnc.sh
+	else
+		chmod +x /ext/source/build/run-scripts/run-sil-ocpp201.sh
+		/ext/source/build/run-scripts/run-sil-ocpp201.sh
+	fi
+fi
